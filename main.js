a81b226c132cd6a4563d5d7beef6c35aed78dbe6
vyze <rafaghazali2202@gmail.com>
2025-07-13 11:10:58 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+document.querySelector(".main");
+document.querySelector(".maun");
+document.querySelector(".muun");
+document.querySelector(".muni");
+/* Dom Javascript */
+
+/* type data */
+let nama = "mrafaghazali";
+let umur = 17,
+let status = "pelajar";
+let null = true
+
+/* switch */
+switch (nama) {
+  case 1: console.log("umur");
+  break;
+  case 2: console.log("status");
+  break;
+  default : console.log();
+}
+
+/* looping & loop */
+for (let i = 0; i < 5; i++) {}
+while(condition) {}
+do {} while (condition);
